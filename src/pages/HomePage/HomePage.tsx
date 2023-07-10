e81180efcb2ef1c501b7e1550621c46b26b04129
ffaa0100aa <ffaa0100aa@gmail.com>
2023-07-10 20:48:15 +0800

    hw2

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,80 @@
-import React, { FC } from 'react';
+import React, { FC,useState } from 'react';
 import './HomePage.scss';
-
 interface HomePageProps {}
 
-const HomePage: FC<HomePageProps> = () => (
-  <div className="HomePage" data-testid="HomePage">
-    HomePage Component
-  </div>
-);
+
+
+const HomePage: React.FC<HomePageProps> = () => {
+  return (
+    <div className="homepage">
+      <div className="rectangle"> 
+        <span className="Payments">Payments</span>
+        
+        <ParentText text="All-Payments">
+          <ChildText text="All-transactions" />
+        </ParentText>
+        <ParentText text="Fraud-Risk">
+          
+        </ParentText>
+        <ParentText text="Invoices">
+          
+        </ParentText>
+        <ParentText text="Subscriptions">
+      
+        </ParentText>
+        <ParentText text="Quotes">
+      
+        </ParentText>
+        <ParentText text="Payment-Link" className="last-parent-text">
+
+        </ParentText>
+      </div> 
+    </div>
+  );
+};
+
+interface ParentTextProps {
+  text: string;
+  children: React.ReactNode;
+  className?: string;
+}
+
+const ParentText: React.FC<ParentTextProps> = ({ text, children, className }) => {
+  const [showChildren, setShowChildren] = useState(false);
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setShowChildren(!showChildren);
+    setClicked(!clicked);
+  };
+
+  return (
+    <div className={`parent-text-container ${className ? className : ''}`}>
+      <div className={`line${clicked ? ' clicked' : ''}`}></div>
+      <p className={`parent-text${clicked ? ' clicked' : ''}`} onClick={handleClick}>
+        {text}
+      </p>
+      {showChildren && <div className="child-container">{children}</div>}
+    </div>
+  );
+};
+
+interface ChildTextProps {
+  text: string;
+}
+
+const ChildText: React.FC<ChildTextProps> = ({ text }) => {
+  const [childClicked, setChildClicked] = useState(false);
+
+  const handleChildClick = () => {
+    setChildClicked(!childClicked);
+  };
+
+  return (
+    <p className={`child-text${childClicked ? ' clicked' : ''}`} onClick={handleChildClick}>
+      {text}
+    </p>
+  );
+};
 
 export default HomePage;
