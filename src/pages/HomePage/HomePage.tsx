bfc2f6ee05a6385f1c789bd26ae1bce76136cc27
Olive020 <a0977062020@gmail.com>
2023-07-10 21:55:07 +0800

    re

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,9 @@
 import React, { FC } from 'react';
 import './HomePage.scss';
-import ButtonBar from '../../components/ButtonBar/ButtonBar'
 interface HomePageProps {}
 
 const HomePage: FC<HomePageProps> = () => (
   <div className="HomePage" data-testid="HomePage">
-    <ButtonBar/>
   </div>
 );
 
